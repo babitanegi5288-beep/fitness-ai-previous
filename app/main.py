dc00add1217a16684f98d933034d86dff73831b5
Babita Negi <babitanegi5288@gmail.com>
2026-09-11 17:15:11 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,9 +19,11 @@ app = FastAPI(title="FitAI Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://fitness-ai-azure.vercel.app",
+]
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
